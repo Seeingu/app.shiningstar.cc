242bbd76c5a1d865099ed6179424bda69b91bac1
cu <seeingasu@gmail.com>
2023-03-23 17:41:43 +0800

    WIP: add plan app

--- app/ui/HStack.tsx
+++ app/ui/HStack.tsx
@@ -1,18 +1,19 @@
-import { CSSProperties, useMemo } from "react"
+import { CSSProperties, HTMLAttributes, useMemo } from "react"
 import { StackStyleProps, useStackStyle } from "./Stack/useStackStyle"
 
-interface Props extends StackStyleProps {
+interface Props extends StackStyleProps, HTMLAttributes<HTMLDivElement> {
     children: React.ReactNode
 }
-export const HStack = ({ children, alignment }: Props) => {
+export const HStack = ({ children, alignment, ...props }: Props) => {
     const style = useStackStyle({
         display: 'flex',
         flexDirection: 'row',
         justifyContent: 'space-around',
-        width: '100%'
+        width: '100%',
+        ...props.style
     }, { alignment })
 
-    return <div style={style}>
+    return <div {...props} style={style}>
         {children}
     </div>
 }
